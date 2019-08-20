64c09e0a5852e01d81907f9ebc7015496960fd85
KMV <KMV@KMV-PC>
2019-08-20 10:41:43 +0530

    first commit

---
+++ src/Queryhelper/java.java
@@ -0,0 +1,18 @@
+  /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Queryhelper;
+import java.sql.*;
+import javax.sql.*;
+
+
+/**
+ *
+ * @author Rakesh
+ */
+public class java {
+    public static void main(string args[])
+    
+}
